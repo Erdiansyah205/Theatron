bd34937fe5ad9fed06ab7a27d9c84e11afc38be9
Erdiansyah205 <erdian054@gmail.com>
2024-03-14 22:59:25 +0700

    perbaikan

---
+++ js/sricpt.js
@@ -0,0 +1,7 @@
+// toggle clas active
+const navbarNav = document.querySelector('.navbar-nav')
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
